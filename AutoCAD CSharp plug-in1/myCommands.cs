b2c348db61398e179c0650e67b2455d43aa40f0a
a7mad799 <a7mad.1.2008@gmail.com>
2020-07-21 21:14:21 +0300

    few bug fixes

--- AutoCAD CSharp plug-in1/myCommands.cs
+++ AutoCAD CSharp plug-in1/myCommands.cs
@@ -100,8 +100,8 @@ namespace AutoCAD_CSharp_plug_in1
                                 coords = matches[2].Value.Split(',');
 
                                 Line connector = new Line(point1, point2);
-                                loadForConnectors.Add(int.Parse(coords[0]));
-                                connector.ColorIndex = int.Parse(coords[1]) + 3;
+                                loadForConnectors.Add((int) double.Parse(coords[0]));
+                                connector.ColorIndex = (int) double.Parse(coords[1]) + 3;
                                 connectors.Add(connector);
                             }
 
@@ -307,7 +307,6 @@ namespace AutoCAD_CSharp_plug_in1
 
                         foreach (int _case in distinctCases)
                         {// loadAvg * lengthAvg / 10
-
                             caseCapacity = 0;
                             caseLengthAvg.Add(0);
                             foreach (Curve connector in connectors)
@@ -316,14 +315,12 @@ namespace AutoCAD_CSharp_plug_in1
                                 {
                                     caseCapacity++;
                                     caseLengthAvg[_case] += connector.GetDistanceAtParameter(connector.EndParam - connector.StartParam);
-                                    //int w = 0;
+                                    // calculating the average total for each case 
                                 }
-
                             }
                             caseLengthAvg[_case] /= caseCapacity;
                             loads[_case] /= caseCapacity;
                             caseGreenTime.Add((int)(caseLengthAvg[_case] * loads[_case] / 40) + 5);
-
                         }
 
                         ed.WriteMessage("\nPlease enter the needed time ");
@@ -1062,7 +1059,27 @@ namespace AutoCAD_CSharp_plug_in1
 
                     pedesRoutesToWrite = pedesRoutesToWrite + "</pedestrain_routes>\n";
 
-                    foreach (int _case in distinctCases)
+                    int[] arr = distinctCases.ToArray();
+                    int[] green = caseGreenTime.ToArray();
+
+                    int temp1, temp2;
+                    for (int j = 0; j <= arr.Length - 2; j++)
+                    {
+                        for (int i3 = 0; i3 <= arr.Length - 2; i3++)
+                        {
+                            if (green[i3] < green[i3 + 1])
+                            {
+                                temp1 = arr[i3 + 1];
+                                arr[i3 + 1] = arr[i3];
+                                arr[i3] = temp1;
+                                temp2 = green[i3 + 1];
+                                green[i3 + 1] = green[i3];
+                                green[i3] = temp2;
+                            }
+                        }
+                    }
+
+                    foreach (int _case in arr)
                     {
                         ed.WriteMessage("for the " + Color.FromColorIndex(ColorMethod.ByColor, (short)(_case + 3)).ColorNameForDisplay +
                                                                 " routes we need: " + caseGreenTime[_case] + " seconds\n");
